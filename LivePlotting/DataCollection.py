b2e0420e8a205a3164132ce01ee2f5d22d451c99
anderslossius1 <101594445+anderslossius1@users.noreply.github.com>
2024-02-09 11:26:18 -0500

    dave

--- LivePlotting/DataCollection.py
+++ LivePlotting/DataCollection.py
@@ -6,3 +6,19 @@ Created on Fri Feb  9 11:07:55 2024
 @author: alossius
 """
 
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+'''
+GOALS
+- Stream live data from two unicorn headsets in parallel
+    - start with conencting 1 headset and steaming data
+    - try to connect another and also stream
+- Process the data live and in parallel
+- Plot the data live and in parallel
+'''
+
+#%% Step 1 - Connect to the UNICORN headset
+
